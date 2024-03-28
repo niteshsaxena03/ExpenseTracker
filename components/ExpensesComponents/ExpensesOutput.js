5f5a5ae86f537e1e8c8f83ea718a53bfb960e0a0
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-28 12:17:19 +0530

    Creating multiple components in a seperate folder so that they can be used later

---
+++ components/ExpensesComponents/ExpensesOutput.js
@@ -0,0 +1,10 @@
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
