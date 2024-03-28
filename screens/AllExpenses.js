d649be619c22641a813ec753f06de66df8ab566b
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-28 12:24:11 +0530

    Modifying the screens to use the expenses componets to display the data

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,7 +1,7 @@
-import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesComponents/ExpensesOutput";
 
 function AllExpenses() {
-  return <Text>AllExpenses Screen</Text>;
+  return <ExpensesOutput expensesPeriod="Total" />;
 }
 
 export default AllExpenses;
