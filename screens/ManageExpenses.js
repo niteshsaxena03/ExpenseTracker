2772658cb6eaf3a1c6cf65258c0687ed49c762e8
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-30 12:27:26 +0530

    Creating a delete button for manage expense screen and applying various styles to it

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,18 +1,49 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId; //to check whether expense id is present or not
   const isEditing = !!editedExpenseId; //trick to convert into boolean
 
-  useLayoutEffect(()=>{
-     navigation.setOptions({
-    title: isEditing ? "Edit expense" : "Add expense", //to dynamically set title based on conditions
-  });
-  },[navigation,isEditing]);
-  
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit expense" : "Add expense", //to dynamically set title based on conditions
+    });
+  }, [navigation, isEditing]);
 
-  return <Text>ManageExpense Screen</Text>;
+  function deleteExpenseHandler() {}
+  return (
+    <View style={styles.container}>
+      {/*we will use condtional rendering here*/}
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            size={36}
+            color={GlobalStyles.colors.error500}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
