68b9fd3d213b203c901e37e5f0766504ead702a8
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-30 12:19:01 +0530

    Adding a functionality to dynamically display the title of manage expense screen by passing id and use route and navigation

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,6 +1,17 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId; //to check whether expense id is present or not
+  const isEditing = !!editedExpenseId; //trick to convert into boolean
+
+  useLayoutEffect(()=>{
+     navigation.setOptions({
+    title: isEditing ? "Edit expense" : "Add expense", //to dynamically set title based on conditions
+  });
+  },[navigation,isEditing]);
+  
+
   return <Text>ManageExpense Screen</Text>;
 }
 
