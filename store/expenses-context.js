c1e09380eb9f0fbf4c9521fecd281ae1dc113ffb
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-31 13:24:14 +0530

    Creating 3 functions in order to manage state

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const ExpenseContext = createContext({
   expenses: [],
@@ -6,3 +6,29 @@ export const ExpenseContext = createContext({
   deleteExpense: (id) => {},
   updateExpense: (id, { description, amount, date }) => {},
 });
+
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+    case "UPDATE":
+    case "DELETE":
+    default:
+      return state;
+  }
+}
+
+function ExpenseContextProvider({ children }) {
+  const [expensesState, dispatch] = useReducer(expensesReducer);
+
+  function addExpense({ expenseData }) {
+    dispatch({ type: "ADD", payload: expenseData });
+  }
+  function deleteExpense({ id, expenseData }) {
+    dispatch({ type: "DELETE", payload: id });
+  }
+  function updateExpense({ id, expenseData }) {
+    dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
+  }
+
+  return <ExpenseContext.Provider>{children}</ExpenseContext.Provider>;
+}
