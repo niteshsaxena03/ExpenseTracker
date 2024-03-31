29a7fd39c08ceacd812f8bd935cfb23e99c290dd
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-31 13:07:29 +0530

    Creating a store folder to manage state using context api

---
+++ store/expenses-context.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+export const ExpenseContext = createContext({
+  expenses: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
+});
