c823ac889ece4f50f336374d1def7d6580581d93
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-04 13:03:57 +0530

    Creating a function to fetch expenses using axios and using it in recentExpenses by useEffect

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,19 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ExpensesOutput from "../components/ExpensesComponents/ExpensesOutput";
 import { ExpenseContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../utils/date";
+import { fetchExpenses } from "../utils/http";
 
 function RecentExpenses() {
   const expensesContext = useContext(ExpenseContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses=await fetchExpenses();
+    }
+    getExpenses();
+  }, []);
 
   //writing logic to get expenses of last 7 days
   const recentExpenses = expensesContext.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
-    return (expense.date>=date7DaysAgo) && (expense.date<=today);
+    return expense.date >= date7DaysAgo && expense.date <= today;
   });
 
-  return <ExpensesOutput expensesPeriod="Last 7 Days" expenses={recentExpenses} fallBackText="No Expenses for last 7 days"/>;
+  return (
+    <ExpensesOutput
+      expensesPeriod="Last 7 Days"
+      expenses={recentExpenses}
+      fallBackText="No Expenses for last 7 days"
+    />
+  );
 }
 
 export default RecentExpenses;
--- utils/http.js
+++ utils/http.js
@@ -1,8 +1,24 @@
 import axios from "axios";
 
+const Backend_Url = "https://expense-tracker-caeba-default-rtdb.firebaseio.com";
+
 export function storeExpense(expenseData) {
-  axios.post(
-    "https://expense-tracker-caeba-default-rtdb.firebaseio.com/expenses.json",
-    expenseData
-  );
+  axios.post(Backend_Url + "/expenses.json", expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(Backend_Url + "/expenses.json");
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObject = {
+      id: key,
+      amount: response.data[key].amount,
+      date:new Date(response.data[key].date),
+      description:response.data[key].description,
+    };
+    expenses.push(expenseObject);
+  }
+  return expenses;
 }
